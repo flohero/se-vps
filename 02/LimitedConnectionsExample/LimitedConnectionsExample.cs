8f3a6bd211278a13ef842adeab4d70f0d4341b65
florian weingartshofer <florian.weingartshofer@tuta.io>
2021-12-03 21:34:18 +0100

    Update LimitedConnections

--- 02/LimitedConnectionsExample/LimitedConnectionsExample.cs
+++ 02/LimitedConnectionsExample/LimitedConnectionsExample.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace LimitedConnectionsExample
 {
@@ -10,39 +11,50 @@ namespace LimitedConnectionsExample
         public static void Main()
         {
             var l = new LimitedConnectionsExample();
-            l.DownloadFiles(new[]
+            l.DownloadFilesAsync(new[]
                 {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"});
+            Console.WriteLine("Hello");
         }
 
         public void DownloadFilesAsync(IEnumerable<string> urls)
         {
-            if (!ThreadPool.SetMaxThreads(10, 10))
-                throw new Exception("Cannot set min/max threads");
-            foreach (var url in urls)
+            var downloadThread = new Thread(() =>
             {
-                ThreadPool.QueueUserWorkItem(DownloadFile, url);
-            }
+                var sem = new Semaphore(10, 10);
+                foreach (var url in urls)
+                {
+                    sem.WaitOne();
+                    var thread = new Thread(() =>
+                    {
+                        DownloadFile(url);
+                        sem.Release();
+                    });
+                    thread.Start();
+                }
+            });
+            downloadThread.Start();
         }
 
         public void DownloadFiles(IEnumerable<string> urls)
         {
-            var toProcess = urls.Count();
-            using ManualResetEvent resetEvent = new ManualResetEvent(false);
+            ICollection<Thread> threads = new List<Thread>();
             foreach (var url in urls)
             {
-                ThreadPool.QueueUserWorkItem(x =>
-                {
-                    DownloadFile(x);
-                    if (Interlocked.Decrement(ref toProcess) == 0)
-                        resetEvent.Set();
-                }, url);
+                var thread = new Thread(() => DownloadFile(url));
+                thread.Start();
+                threads.Add(thread);
             }
 
-            resetEvent.WaitOne();
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
         }
 
         private void DownloadFile(object url)
         {
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine(url);
             // download and store file ... 
         }
     }
